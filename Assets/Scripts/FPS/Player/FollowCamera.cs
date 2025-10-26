850e7abe0eb7d044396812220d0f5f6f803dbbf4
NoahDevlop <133510898+NoahDevlop@users.noreply.github.com>
2025-10-26 15:48:16 +0100

    Lerp Weapon

---
+++ Assets/Scripts/FPS/Player/FollowCamera.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+
+    public bool followPosition = true;
+    public bool followRotation = true;
+
+    public Vector3 positionOffset = Vector3.zero;
+    public Vector3 rotationOffsetEuler = Vector3.zero;
+
+    public bool smoothPosition = false;
+    public float positionSmoothSpeed = 10f;
+
+    public bool smoothRotation = false;
+    public float rotationSmoothSpeed = 10f;
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        // Position
+        if (followPosition)
+        {
+            Vector3 desiredPos = target.position + positionOffset;
+            if (smoothPosition)
+            {
+                transform.position = Vector3.Lerp(transform.position, desiredPos, Time.deltaTime * positionSmoothSpeed);
+            }
+            else
+            {
+                transform.position = desiredPos;
+            }
+        }
+
+        // Rotation
+        if (followRotation)
+        {
+            Quaternion targetRot = target.rotation * Quaternion.Euler(rotationOffsetEuler);
+
+            if (smoothRotation)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * rotationSmoothSpeed);
+            }
+            else
+            {
+                transform.rotation = targetRot;
+            }
+        }
+    }
+
+    public void SnapToTarget()
+    {
+        if (target == null) return;
+
+        if (followPosition)
+        {
+            transform.position =  target.position + positionOffset;
+        }
+        if (followRotation)
+        {
+            transform.rotation = target.rotation * Quaternion.Euler(rotationOffsetEuler);
+        }
+    }
+}
