38d78f45e20f095f07934ab27962f43e30c12c2f
NoahDevlop <133510898+NoahDevlop@users.noreply.github.com>
2025-12-05 22:51:38 +0100

    ArenaScript(work in progress)

--- Assets/Scripts/System/Arena/ArenaSetter.cs
+++ Assets/Scripts/System/Arena/ArenaSetter.cs
@@ -1,12 +1,34 @@
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+
+
+public class Enemies : MonoBehaviour 
+{
+    public GameObject enemyPrefab;
+    public int nbEnnemies;
+}
 
 [CreateAssetMenu(fileName = "ArenaSetter", menuName = "REDACTED_PROJECT_NAME/Arena/ArenaSetter", order = 1)]
 public class ArenaSetter : ScriptableObject
 {
-    public int nbEnnemies;
     public List<Transform> spawnerPoints;
     public List<GameObject> ennemiesPrefabs;
+    public DoorArena door;
+    public bool waveStarted = false;
+
+    [SerializeField] private float delaySpawnBetweenEnemies = 0.5f;
+
+    public void TriggerWave()
+    {
+        waveStarted = true;
+        door.isOpen = false;
+    }
 
 
+
+    private void RandomEnemySelector()
+    {
+        
+    }
 }
